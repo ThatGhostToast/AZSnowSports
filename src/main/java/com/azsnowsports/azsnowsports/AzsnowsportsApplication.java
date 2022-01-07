aeaa3e621539c215884c0eff7d2c046bc1b7536e
Zac Almas <zalmas@my.gcu.edu>
2022-01-07 08:53:53 -0700

    Add files via upload

---
+++ src/main/java/com/azsnowsports/azsnowsports/AzsnowsportsApplication.java
@@ -0,0 +1,13 @@
+package com.azsnowsports.azsnowsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzsnowsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzsnowsportsApplication.class, args);
+	}
+
+}

9eadd27a41ba468a926dc568ef1fd79cc1841438
Zac Almas <zacalmas@gmail.com>
2022-01-30 04:41:18 -0700

    Add files via upload

---
+++ src/test/java/com/azsnowsports/AzSnowSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsnowsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzSnowSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
